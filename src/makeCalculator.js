653c2edb52203e4209daaff16cda5a4d9b6a0e54
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2025-03-02 09:04:34 -0300

    add task solution

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,27 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    operate: function (callback, num) {
+      this.result = callback(this.result, num);
+
+      return this;
+    },
+    add: (num1, num2) => num1 + num2,
+    subtract: (num1, num2) => num1 - num2,
+    multiply: (num1, num2) => num1 * num2,
+    divide: (num1, num2) => num1 / num2,
+
+    reset: function () {
+      this.result = 0;
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
